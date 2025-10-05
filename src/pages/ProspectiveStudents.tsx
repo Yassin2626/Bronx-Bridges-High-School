5c690aa0fbfebbadd7ddc6ad059d9efc0b6fbf20
Yassin2626 <sinogames2626@gmail.com>
2025-10-05 17:48:12 -0400

    New Update

--- src/pages/ProspectiveStudents.tsx
+++ src/pages/ProspectiveStudents.tsx
@@ -299,6 +299,7 @@ const ProspectiveStudents = () => {
                             className="text-black hover:text-yellow-600 underline font-medium transition-colors duration-300"
                           >
                             ZeregaFWC@schools.nyc.gov
+                          </a>
                         </div>
                       </div>
                     </div>
